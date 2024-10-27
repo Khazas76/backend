8bce2ec1248c9f37ec4a429ace40f1c11ff1721e
Kong <nutdanui2003@gmail.com>
2024-10-27 16:30:35 +0700

    Update index.html

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ app.post("/staff-login", (req, res) => {
 app.get("/staff-dashboard", (req, res) => {
     res.json(orders);
 });
-
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'))
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html"); // เปลี่ยนเป็นชื่อไฟล์ของคุณ
 });
